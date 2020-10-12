9b29893f0d3cf666883af60845f6e5f2eedd9e0f
sergeyman <sergey_man@ukr.net>
2020-10-12 14:55:13 +0300

    FC: Adding useEffect, deleting (this, this.state, render, bind(), setId(), selectedId())

--- src/functionalComponents/users/list.js
+++ src/functionalComponents/users/list.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as usersApi from "./../../api/users";
 import UserProfile from "./profile";
 
 //import "./list.css";
 
-export default class extends React.PureComponent {
-  //PureComponent - ?
+export default function () {
+  //let [some, someSet] = useState(0); //useReducer() для больших по объему данных
+  let [users, setUsers] = useState({ loaded: false, list: null });
+  //usersApi.all().then((users) => {}); //NOT CORRECT (side effects)
+
+  //subst all LCM (invoke on rerender comp.)
+  useEffect(() => {
+    usersApi.all().then((list) => setUsers({ loaded: true, list }));
+  });
+
+  /*
   state = {
     loaded: false,
     users: [],
@@ -32,49 +41,67 @@ export default class extends React.PureComponent {
       });
     });
   }
-
-  render() {
-    if (!this.state.loaded) {
-      return <div>Loading...</div>;
-    }
-
-    //let usersList = '';
-    let usersList = this.state.users.map((user) => {
-      let classes = []; // selected user show
-      if (user.id === this.state.selectedId) {
-        classes.push("text-success");
-      }
-      return (
-        <li
-          className={classes.join(" ")}
-          key="user.id"
-          onClick={() => this.setId(user.id)}
-        >
-          {user.name}
-        </li>
-      );
-    });
-
-    // let userInfo = this.state.selectedId;
-    let userInfo =
-      this.state.selectedId === null ? (
-        <div className="alert alert-warning">Please, selected user!</div>
-      ) : (
-        <UserProfile
-          id={this.state.selectedId}
-          //problem 3 solve #1 full reload
-          // key={this.state.selectedId}
-        />
-      );
-
-    return (
-      // <div className="users">
-      <div>
-        <p>Functional compmnents with hooks</p>
-        <ul className="list-group">{usersList}</ul>
-        <hr />
-        {userInfo}
-      </div>
-    );
+*/
+  // render() {
+  // if (!this.state.loaded) {
+  if (!users.loaded) {
+    return <div>Loading...</div>;
+    // return <div>{users.loaded}</div>;
   }
+
+  //let usersList = '';
+  // let usersList = this.state.users.map((user) => {
+  let usersList = users.list.map((user) => {
+    let classes = []; // selected user show
+    //if (user.id === this.state.selectedId) {
+    if (user.id === 0) {
+      classes.push("text-success");
+    }
+    return (
+      <li
+        className={classes.join(" ")}
+        key="user.id"
+        // onClick={() => this.setId(user.id)}
+        onClick={() => {}}
+      >
+        {user.name}
+      </li>
+    );
+  });
+
+  // let userInfo = this.state.selectedId;
+  let userInfo =
+    // this.state.selectedId === null ? (
+    1 === null ? (
+      <div className="alert alert-warning">Please, selected user!</div>
+    ) : (
+      <UserProfile
+        // id={this.state.selectedId}
+        id={null}
+        //problem 3 solve #1 full reload
+        // key={this.state.selectedId}
+      />
+    );
+
+  return (
+    // <div className="users">
+    <div>
+      <p>Functional compmnents with hooks</p>
+      <ul className="list-group">{usersList}</ul>
+      <hr />
+      {userInfo}
+    </div>
+  );
 }
+// }
+
+/*
+CC -> FC
+-render
+-this
+-this.state
+-bind()
+-setId()
+-selectedId
+
+*/
